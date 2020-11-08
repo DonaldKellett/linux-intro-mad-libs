771448aff8d9a1e2721fd5ca8e64c1be447b47dc
Donald Sebastian Leung <donaldsebleung@gmail.com>
2020-11-08 17:25:19 +0800

    Add form for filling in blanks

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,32 @@
 'use strict'
+const mtp = require('madlibs-template-parser')
+const fs = require('fs')
+const path = require('path')
+const gameText = fs.readFileSync(path.join(__dirname, '../game.txt')).toString()
+const template = mtp(gameText)
+const capitalize = str => str[0].toUpperCase() + str.slice(1)
+
+let form = '<form action="/" method="POST">'
+let blankTypes = {}
+for (let paragraph of template)
+  for (let token of paragraph)
+    if (token.type === 'blank') {
+      let id = token.variant
+        .split` `
+	.map((word, idx) => idx === 0 ? word : capitalize(word))
+	.join`` + capitalize(token.category)
+      if (typeof blankTypes[id] === 'undefined')
+	blankTypes[id] = 0
+      else
+	++blankTypes[id]
+      id = id + blankTypes[id]
+      form += `<p><input name="${id}" type="text" placeholder="${capitalize(token.category)} (${token.variant})" value="" /></p>`
+    }
+form += '<p><input name="submit" type="submit" class="primary" value="Madlibify!" /></p>'
+form += '</form>'
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return reply.view('index.hbs', { form: '<p>TODO</p>' })
+    return reply.view('index.hbs', { form })
   })
 }
